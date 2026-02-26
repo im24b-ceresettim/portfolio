96393fb07cdb6583c33e7bdc474c7d4c397c1f06
Matteo Ceresetti <ceresettim@bzz.ch>
2026-02-26 20:29:02 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- app/about_me/page.js
+++ app/about_me/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            This is the home page. Mi casa tu casa.
+            Jep, thats me. You probably wondered how i got in this situation.
           </h1>
         </div>
       </main>
